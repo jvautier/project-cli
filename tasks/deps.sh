30100f4fdd08afc54bb1249ea0941ff7f4058970
Joe Vautier <jonathan.vautier@gmail.com>
2023-04-09 19:50:16 +0000

    feat: structure project

--- tasks/deps.sh
+++ tasks/deps.sh
@@ -1,3 +1,18 @@
 
-pip-compile --output-file requirements.txt requirements.in
-pip-compile --output-file requirements-dev.txt requirements-dev.in
+function _do() {
+    
+pip-compile \
+    --verbose \
+    --rebuild \
+    --upgrade \
+    --no-annotate \
+    --no-emit-trusted-host \
+    --no-emit-index-url \
+    --no-header \
+    --output-file $2 \
+    $1
+}
+
+_do requirements.in requirements.txt
+_do dev/requirements.in dev/requirements.txt
+_do tests/requirements.in tests/requirements.txt
